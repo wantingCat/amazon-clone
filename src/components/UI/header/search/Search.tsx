b04ef307fcf2dadcf27ab3c1d798635c5b8abc49
Tushar Chauhan <ryuzakiakira@protonmail.com>
2024-01-09 15:01:49 +0530

    added search

--- src/components/UI/header/search/Search.tsx
+++ src/components/UI/header/search/Search.tsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import searchIcon from "../../../../assets/logo/search-svgrepo-com.svg";
 
+import { API } from "../../../../utils/api";
+import Product from "../../../../utils/Product";
+
 import classes from "./Search.module.css";
-import { useNavigate } from "react-router-dom";
 
 function Search() {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState<Product[]>([]);
   const navigate = useNavigate();
-  
+
+  const searchInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch =async () => {
+    try {
+      const temp = await fetch(`${API}/search?q=${search}`);
+      const data = await temp.json();
+      setSearchResult(data.products);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const sendData = () => {
+    navigate("/products/search", { state: { data: searchResult } });
+  }
+
   const onSubmitHandler = (e: React.FormEvent) => {
     e.preventDefault();
-    navigate("/products/search");
+    getSearch();
+
+    setTimeout(() => {
+      sendData();
+    }, 1000);
   };
+
   return (
     <div>
       <search className={classes.search}>
@@ -21,6 +49,8 @@ function Search() {
               id="search"
               name="search"
               placeholder="Search..."
+              value={search}
+              onChange={searchInputHandler}
             />
             <button type="submit">
               <img src={searchIcon} alt="search" height={15} />
--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,4 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 function SearchResults() {
+    const location = useLocation();
+    const data = location.state.data;
+
+    console.log(data);
+
     return (
         <h1>results</h1>
     )
